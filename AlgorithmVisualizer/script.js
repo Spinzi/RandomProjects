cebacda1a9b078dd8f1f9b35f8f28ec170dc05df
Spinzi <alinnicusorgisca@gmail.com>
2026-03-10 20:04:15 +0200

    Habibi

---
+++ AlgorithmVisualizer/script.js
@@ -0,0 +1,10 @@
+//SMatrix Engine State
+let matrix = [];
+let selectedCell = { x: null, y: null};
+
+// DOM Elements
+const wrapper = document.getElementById('matrix-wrapper');
+const coordDisplay = document.getElementById('coords');
+const editInput = document.getElementById('edit-val');
+const btnGenerate = document.getElementById('btn-generate');
+
